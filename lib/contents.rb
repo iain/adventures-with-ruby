4d28b52b3c7c349e55c2911e1d755d77305b452d
iain <iain@iain.nl>
2011-10-16 23:20:07 +0200

    Reading compiled yaml contents

--- lib/contents.rb
+++ lib/contents.rb
@@ -1,3 +1,32 @@
+require 'yaml'
+
 class Contents
+
+  def initialize(id, path = "contents")
+    @id   = id
+    @path = path
+  end
+
+  def html
+    parsed_yaml.fetch("html")
+  end
+
+  def toc
+    parsed_yaml.fetch("toc")
+  end
+
+  def introduction
+    parsed_yaml.fetch("introduction")
+  end
+
+  private
+
+  def parsed_yaml
+    YAML.load_file(filename)
+  end
+
+  def filename
+    File.join(@path, "#{@id}.yml")
+  end
 
 end
--- spec/lib/contents_spec.rb
+++ spec/lib/contents_spec.rb
@@ -2,19 +2,35 @@ require 'contents'
 
 describe Contents do
 
-  describe "#toc?" do
+  let(:path) { "spec" }
+
+  subject { Contents.new("compiled-article", path) }
+
+  describe "#html" do
+
+    it "reads html from the compiled file" do
+      subject.html.should == "Compiled HTML"
+    end
 
   end
 
   describe "#toc" do
 
-  end
-
-  describe "#html" do
+    it "reads table of contents from the compiled file" do
+      subject.toc.should == [ { "anchor" => "#html+anchor", "title" => "HTML title" } ]
+    end
 
   end
 
   describe "#introduction" do
+
+    it "reads the introduction from the compiled file" do
+      subject.introduction.should == "Compiled introduction"
+    end
+
+  end
+
+  describe "#toc?" do
 
   end
 
