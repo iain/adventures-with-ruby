6207cbe7b30b1c6bdd28c3419f50ad767e4c843a
iain <iain@iain.nl>
2011-10-16 18:56:02 +0200

    Read the index file

--- lib/index_reader.rb
+++ lib/index_reader.rb
@@ -1,7 +1,24 @@
+require 'yaml'
+
 class IndexReader
 
   def self.read(filename = "articles/_index.yml")
     new(filename).read
+  end
+
+  def initialize(filename, article = Article)
+    @filename = filename
+    @article  = article
+  end
+
+  def read
+    Hash[ load_file.map { |k,v| [ k, @article.new(v) ] } ]
+  end
+
+  private
+
+  def load_file
+    YAML.load_file(@filename)
   end
 
 end
--- spec/lib/index_reader_spec.rb
+++ spec/lib/index_reader_spec.rb
@@ -3,6 +3,7 @@ require 'index_reader'
 describe IndexReader do
 
   describe ".read" do
+
     it "delegates to instance read with default file" do
       reader = double
       filename = "articles/_index.yml"
@@ -10,10 +11,26 @@ describe IndexReader do
       reader.should_receive(:read)
       IndexReader.read
     end
+
   end
 
   describe "#read" do
-    it "reads the file"
+
+    let(:article_class) { double("ArticleClass") }
+
+    subject { IndexReader.new("spec/index.yml", article_class) }
+
+    it "makes an article instance" do
+      article = { "title" => "Article Name", "publish" => Date.new(2011, 10, 15), "summary" => "Summary" }
+      article_class.should_receive(:new).with(article)
+      subject.read
+    end
+
+    it "reads the file" do
+      article = double("article")
+      article_class.stub(:new) { article }
+      subject.read.should == { "article-name" => article }
+    end
   end
 
 end
