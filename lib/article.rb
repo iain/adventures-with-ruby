e9edba7437e218ba0880dbc1c2f18aef7a2f16a3
iain <iain@iain.nl>
2011-10-16 20:50:30 +0200

    Article implementation

--- lib/article.rb
+++ lib/article.rb
@@ -1,11 +1,38 @@
 class Article
 
+  attr_reader :id
+
   def initialize(id, attributes)
+    @id         = id
     @attributes = attributes
+  end
+
+  def found?
+    true
   end
 
   def title
     @attributes["title"]
+  end
+
+  def deprecated?
+    @attributes["deprecated"]
+  end
+
+  def summary
+    @attributes["summary"]
+  end
+
+  def url
+    "/#{id}"
+  end
+
+  def old?(oldness = Oldness)
+    oldness.old?(@attributes["publish"])
+  end
+
+  def contents(contents = Contents)
+    contents.read(id)
   end
 
 end
--- spec/lib/article_spec.rb
+++ spec/lib/article_spec.rb
@@ -2,10 +2,49 @@ require 'article'
 
 describe "Article" do
 
-  it "allows for attributes" do
-    attributes = { "title" => "Article Name", "publish" => Date.new(2011, 10, 15), "summary" => "Summary" }
-    article = Article.new("article-name", attributes)
-    article.title.should == "Article Name"
+  let(:id) { "article-name" }
+  let(:publish) { Date.new(2011, 10, 15) }
+  let(:summary) { "The Summary" }
+  let(:attributes) { { "title" => "Article Name", "publish" => publish, "summary" => summary } }
+
+  subject { Article.new(id, attributes) }
+
+  it "has an id" do
+    subject.id.should == id
+  end
+
+  it "has a title" do
+    subject.title.should == "Article Name"
+  end
+
+  it { should be_found }
+
+  it "can be old" do
+    oldness = mock
+    oldness.should_receive(:old?).with(publish)
+    subject.old?(oldness)
+  end
+
+  it "has a summary" do
+    subject.summary.should == summary
+  end
+
+  it "has a url" do
+    subject.url.should == "/article-name"
+  end
+
+  it "has contents" do
+    contents = mock
+    contents.should_receive(:read).with(id)
+    subject.contents(contents)
+  end
+
+  it "might not be deprecated" do
+    subject.should_not be_deprecated
+  end
+
+  it "might be deprecated" do
+    Article.new(id, attributes.merge("deprecated" => true)).should be_deprecated
   end
 
 end
