d6a4db4061df45736943569ccf8646a0721001eb
iain <iain@iain.nl>
2011-10-21 18:44:18 +0200

    Introduction parsing

---
+++ lib/introduction.rb
@@ -0,0 +1,44 @@
+require 'redcarpet'
+
+class Introduction < Redcarpet::Render::HTML
+
+  def self.read(text)
+    md = Redcarpet::Markdown.new(Introduction.new, :fenced_code_blocks => true)
+    md.render(text)
+  end
+
+  MARK = "<!-- INTRODUCTION STOPS HERE -->"
+
+  def initialize(*)
+    super
+    @number_of_paragraphs = 0
+  end
+
+  def header(*)
+    MARK
+  end
+
+  def paragraph(text)
+    if @number_of_paragraphs >= 3
+      MARK
+    elsif text.to_s == ""
+      nil
+    else
+      @number_of_paragraphs += 1
+      "<p>#{text}</p>"
+    end
+  end
+
+  def block_code(*)
+    MARK
+  end
+
+  def image(*)
+    ""
+  end
+
+  def postprocess(full_document)
+    full_document.split(MARK, 2).first
+  end
+
+end
---
+++ spec/lib/introduction_spec.rb
@@ -0,0 +1,75 @@
+require 'introduction'
+
+describe "Introduction" do
+
+  it "gets the contents before the first header" do
+    text = <<-TEXT
+First paragraph
+
+Second paragraph
+
+## Header
+
+Third paragraph
+    TEXT
+
+    intro(text).should == "<p>First paragraph</p><p>Second paragraph</p>"
+  end
+
+  it "removes images" do
+
+    text = <<-TEXT
+First paragraph
+
+![alt](image.png)
+
+Second paragraph
+
+## Header
+
+Third paragraph
+    TEXT
+
+    intro(text).should == "<p>First paragraph</p><p>Second paragraph</p>"
+
+  end
+
+  it "stops before code blocks" do
+
+    text = <<-TEXT
+First paragraph
+
+Second paragraph
+
+```
+some code
+```
+
+## Header
+
+Third paragraph
+    TEXT
+
+    intro(text).should == "<p>First paragraph</p><p>Second paragraph</p>"
+  end
+
+  it "limits to three paragraphs" do
+
+    text = <<-TEXT
+First paragraph
+
+Second paragraph
+
+Third paragraph
+
+Fourth paragraph
+    TEXT
+
+    intro(text).should == "<p>First paragraph</p><p>Second paragraph</p><p>Third paragraph</p>"
+  end
+
+  def intro(text)
+    Introduction.read(text)
+  end
+
+end
